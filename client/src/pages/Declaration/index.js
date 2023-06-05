2af5ecf5efd01dd89a1802dca736bae4460038c0
chanyoungcoding <white1614@naver.com>
2023-06-05 18:45:25 +0900

    Declaration 부분 수정

--- client/src/pages/Declaration/index.js
+++ client/src/pages/Declaration/index.js
@@ -3,30 +3,20 @@ import './declaration.css';
 
 const Index = () => {
   return (
-    <div>
-      <h1>
-        RED
-      </h1>
-
-      <h1>
-        BLUE
-      </h1>
-
-      <h1>
-        Yellow
-      </h1>
-
-      <h1>
-        GREEN
-      </h1>
-
-      <h1>
-        ORANGE
-      </h1>
-
-      <h1>
-        VIOLET
-      </h1>
+    <div className='declaration__text'>
+      <p className='red__text'>
+        <span className='violet__text'>import</span>
+        &nbsp;&nbsp;time&nbsp;&nbsp;
+        <span className='violet__text'>from</span>
+        &nbsp;&nbsp;life
+      </p>
+      <div className='text__ascent'>
+        <p>
+          <span className='yellow__text'>time</span>
+          <span className='blue__text'>heal</span>
+          <span className='orange__text'>&#40;&nbsp;&#41;</span>
+        </p>
+      </div>
     </div>
   );
 };
